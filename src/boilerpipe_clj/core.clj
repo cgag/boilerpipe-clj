4574061b2050d88c7729d6879ed8d3d25dd3c963
Nick Barnwell <nb@ul.io>
2013-03-28 12:06:07 -0700

    Simplify and unify get-text method

--- src/boilerpipe_clj/core.clj
+++ src/boilerpipe_clj/core.clj
@@ -1,7 +1,5 @@
 (ns boilerpipe-clj.core
-  (:require [clojure.string :as s]
-            [clj-http [client  :as http]
-                      [cookies :as cookies]])
+  (:require [clojure.string :as s])
   (:import de.l3s.boilerpipe.extractors.ArticleExtractor
            de.l3s.boilerpipe.extractors.DefaultExtractor))
 
@@ -10,36 +8,17 @@
 (def article-extractor (ArticleExtractor/getInstance))
 (def default-extractor (DefaultExtractor/getInstance))
 
-(defprotocol TextExtractor
-  "Extract the text from a given source"
-  (extract-text [source extractor] "Get text from a given source using "))
-
-(extend-protocol TextExtractor
-  java.net.URL
-  (extract-text [source extractor]
-    (try
-      (let [cookie-store (cookies/cookie-store)
-            resp (http/get (str source) {:cookie-store cookie-store
-                                         :headers {"User-Agent" *user-agent*}})
-            body-str (:body resp)]
-        (extract-text body-str extractor))
-      (catch Exception e
-        (str "Error fetching " source))))
-  Object
-  (extract-text [source extractor]
-    (.getText extractor source)))
-
 (defn get-text
   "text-source can be a java.net.URL, a String, a Reader, etc"
   [source & [extractor]]
-  (let [extractor (or extractor article-extractor)]
-    (extract-text source extractor)))
+  (let [extractor (or extractor article-extractor)
+        source (slurp source)]
+    (.getText extractor source)))
 
 (declare wrap-paragraphs)
 
 (defn get-html [source & [extractor]]
-  (let [text (get-text source extractor)]
-    (wrap-paragraphs text)))
+  (-> (get-text source extractor) wrap-paragraphs))
 
 (defn wrap-paragraphs 
   "Wraps the paragraphs of the text in <p> tags."
@@ -48,9 +27,3 @@
     (->> paragraphs
       (map #(str "<p>" % "</p>"))
       (s/join ""))))
-
-(defn get-url-text [url]
-  (try
-    (get-text (java.net.URL. url))
-    (catch Exception e
-      (str "Invalid url: " url))))
