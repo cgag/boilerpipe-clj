b870aa34b3960918e5dd50ba08c542670b471045
Nick Barnwell <nb@ul.io>
2013-03-29 23:44:20 -0700

    Import Java classes the Hickey-approved™ way

--- src/boilerpipe_clj/extractors.clj
+++ src/boilerpipe_clj/extractors.clj
@@ -1,6 +1,7 @@
 (ns boilerpipe-clj.extractors 
-  (:import de.l3s.boilerpipe.extractors.ArticleExtractor
-           de.l3s.boilerpipe.extractors.DefaultExtractor))
+  (:import (de.l3s.boilerpipe.extractors 
+             ArticleExtractor
+             DefaultExtractor)))
 
 (defonce article-extractor (ArticleExtractor/getInstance))
 (defonce default-extractor (DefaultExtractor/getInstance))
