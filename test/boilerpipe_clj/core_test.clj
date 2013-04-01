5f755c01edc645e0355a0e6e9315a5b79bc31995
Curtis Gagliardi <gagliardi.curtis@gmail.com>
2013-04-01 01:02:22 -0500

    Add more tests.

--- test/boilerpipe_clj/core_test.clj
+++ test/boilerpipe_clj/core_test.clj
@@ -1,13 +1,29 @@
 (ns boilerpipe-clj.core-test
-  (:use clojure.test
-        boilerpipe-clj.core))
+  (:require [clojure.test :refer :all]
+            [boilerpipe-clj.core :refer :all]
+            [boilerpipe-clj.extractors :as ext]))
 
 (defonce test-article 
   (slurp "./test/boilerpipe_clj/resources/greenspun-test.html"))
 
+(def extractors [ext/default-extractor
+                 ext/article-extractor
+                 ext/article-sentence-extractor])
+
+;; TODO: Why does lein test say it tests 6400 assertions?
 (deftest get-text-extraction
-  (testing "that get-text extracts something from an article"
+  (testing "get-text extracts something from an article
+            without a provided extractor"
     (let [res (get-text test-article)] 
       (is (and 
             (not= res "")
-            (not= res nil))))))
+            (not= res nil)))))
+  (testing "All the available extractors extract something
+           from the article"
+    (doseq [ext extractors
+            res (get-text test-article ext)]
+      (is (and (not= res "")
+               (not (nil? res)))))))
+
+;; TODO: Test wrap-paragraphs and get-text-as-html
+;; TODO: More fine grained tests for the extractors.  Not sure what these tests would look like.
