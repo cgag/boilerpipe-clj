85f651f1b3439e0bffd80dff6b9b5db59deee1a6
Curtis Gagliardi <gagliardi.curtis@gmail.com>
2013-04-06 01:06:03 -0500

    Use latest version of clojure

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject io.curtis/boilerpipe-clj "0.2.0"
+(defproject io.curtis/boilerpipe-clj "0.2.1-SNAPSHOT"
   :description "A simple wrapper around the Boilerpipe library for extracting text from html articles/pages"
   :url "http://curtis.io"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.thnetos/cd-client "0.3.4" :exclusions [org.clojure/clojure cheshire]]
                  [net.sourceforge.nekohtml/nekohtml "1.9.15"]
                  [xerces/xercesImpl "2.9.1"]
