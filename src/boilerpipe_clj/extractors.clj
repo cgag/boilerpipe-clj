24e9045251afe4f482199f02b66a6684888b737b
Ping <seymores@github.com>
2014-09-23 11:21:41 +0800

    Added get-images

--- src/boilerpipe_clj/extractors.clj
+++ src/boilerpipe_clj/extractors.clj
@@ -1,10 +1,12 @@
-(ns boilerpipe-clj.extractors 
-  (:import (de.l3s.boilerpipe.extractors 
+(ns boilerpipe-clj.extractors
+  (:import (de.l3s.boilerpipe.extractors
              ArticleExtractor
              ArticleSentencesExtractor
-             DefaultExtractor)))
+             DefaultExtractor)
+             (de.l3s.boilerpipe.sax ImageExtractor)))
 
 (defonce article-extractor (ArticleExtractor/getInstance))
 (defonce default-extractor (DefaultExtractor/getInstance))
+(defonce image-extractor (ImageExtractor/INSTANCE))
 (defonce article-sentence-extractor
   (ArticleSentencesExtractor/getInstance))
